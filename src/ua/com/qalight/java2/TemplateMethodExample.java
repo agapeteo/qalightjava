059353570b44ddfef434e3f46f09e7b57219ae40
Alexander Gandzha <agapeteo@gmail.com>
2014-05-03 14:57:54 -0400

    updated templete method example with Cow

--- src/ua/com/qalight/java2/TemplateMethodExample.java
+++ src/ua/com/qalight/java2/TemplateMethodExample.java
@@ -7,127 +7,99 @@ import java.util.*;
 
 public class TemplateMethodExample {
 
-    static class Person {
-        public void doAllSteps(){
-            doFirst();
-            doSecond();
-            doThird();
-        }
-
-        public void doFirst(){
-            System.out.println("first step finished");
-        }
-
-        public void doSecond(){
-            System.out.println("first second as template");
-        };
-
-        public void doThird(){
-            System.out.println("third step finished");
-        }
-    }
-    static class Employee extends Person{
-
-        @Override
-        public void doSecond() {
-            System.out.println("I'm doing second step as employee");
-        }
-    }
-
-    static class Child extends Person{
-        @Override
-        public void doSecond() {
-            System.out.println("I'm doing second step as child");
-        }
-        void playAsChild(){
-
-        }
-    }
-
     class Animal{
+        protected void life() {
+            eat();
+        }
+
         protected void eat(){
             System.out.println("eating");
-        };
+        }
+
     }
     class Fish extends Animal{
+        @Override
+        protected void life(){
+            super.life();
+            swim();
+        }
         protected void swim() {
             System.out.println("I'm swiming");
         }
     }
 
     class Piranha extends Fish{
+        @Override
+        protected void life(){
+            super.life();
+            bite();
+        }
         protected void bite(){
             System.out.println("I can bite");
         }
     }
 
-    class Bird extends Animal{
-        protected void fly(){
+    class Bird extends Animal {
+        protected void fly() {
             System.out.println("I can fly");
         }
+
+        @Override
+        protected void life() {
+            super.life();
+            fly();
+        }
     }
+
     class Duck extends Bird{
+        @Override
+        protected void life(){
+            super.life();
+            dive();
+        }
         protected void dive(){
             System.out.println("I can dive");
         }
     }
 
+    class Mammal extends Animal{
+        protected void feedMilk(){
+            System.out.println("I can feed milk");
+        }
+
+        @Override
+        protected void life(){
+            super.life();
+            feedMilk();
+        }
+    }
+
+    class Cow extends Mammal{
+        protected void giveMilk(){
+            System.out.println("I can also give a milk to people");
+        }
+
+        @Override
+        public void life(){
+            super.life();
+            giveMilk();
+        }
+
+    }
+
     @Test
     public void animalLife() {
-        List<Animal> allZooAnimal = Arrays.asList(new Piranha(), new Duck(), new Duck());
+        List<Animal> allZooAnimal = Arrays.asList(new Piranha(), new Duck(), new Duck(), new Cow());
 
         showZooLife(allZooAnimal);
     }
 
     private void showZooLife(Collection<Animal> allZooAnimal) {
-
         for (Animal eachAnimal : allZooAnimal) {
-            System.out.println();
-            System.out.println("next animal of type " + eachAnimal.toString());
-            eachAnimal.eat();
-
-            if (eachAnimal instanceof Fish) {
-                ((Fish) eachAnimal).swim();
-                if (eachAnimal instanceof Piranha) {
-                    ((Piranha) eachAnimal).bite();
-                }
-            }
-
-            if (eachAnimal instanceof Bird) {
-                Bird bird = (Bird) eachAnimal;
-                bird.fly();
-                if (bird instanceof Duck) {
-                    Duck duck = (Duck) bird;
-                    duck.dive();
-                }
-            }
+            System.out.println("next animal is " + eachAnimal.toString());
+            eachAnimal.life();
         }
     }
 
-    static class OldPerson extends Person{
 
-    }
-
-    @Test
-    public void testPerson() {
-        Person employee = new Employee();
-        Person child = new Child();
-        Person oldPerson = new OldPerson();
-
-        List list = new ArrayList();
-        list.add(employee);
-        list.add(child);
-        list.add(oldPerson);
-
-//        List<Person> persons = Arrays.asList(employee, child, oldPerson);
-
-        doSomethingWithPersons(list);
-    }
-
-    void  doSomethingWithPersons(List<Person> persons){
-        for (Person eachPerson : persons){
-            eachPerson.doAllSteps();
-            System.out.println("-------");
-        }
-    }
 }
